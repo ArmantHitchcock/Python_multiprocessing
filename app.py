7022a975b7b0808d9e0c1741371db8b9cb362938
Armant Hitchcock <armant.hitchcock@gmail.com>
2020-04-13 01:31:57 +0200

    app.py processes code sync

---
+++ app.py
@@ -0,0 +1,14 @@
+import time
+
+start = time.perf_counter() #start timer
+
+def doSomething():
+    print('Sleeping 1 second...')
+    time.sleep(1)
+    print('done sleeping..')
+
+doSomething()   #end of timer
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start,2)} second(s)')
