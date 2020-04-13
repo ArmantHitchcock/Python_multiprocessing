50eef0208d7b3381c3bb7fc8c32f020443242445
Armant Hitchcock <armant.hitchcock@gmail.com>
2020-04-13 02:22:51 +0200

    old manual way of multiprocessing

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
+from multiprocessing import Process
 import time
 
 start = time.perf_counter() #start timer
 
-def doSomething():
-    print('Sleeping 1 second...')
-    time.sleep(1)
+def doSomething(seconds):
+    print(f'Sleeping {seconds} second...')
+    time.sleep(seconds)
     print('done sleeping..')
+'''
+p1 = multiprocessing.Process(target=doSomething)
+p2 = multiprocessing.Process(target=doSomething)
 
-doSomething()   #end of timer
+p1.start()
+p2.start()
 
-finish = time.perf_counter()
+p1.join()
+p2.join()
+'''
+Processes = []
 
-print(f'Finished in {round(finish-start,2)} second(s)')
+if __name__ == '__main__':
+    for _ in range(10):
+        p = Process(target=doSomething, args=[1.5])
+        p.start()
+        Processes.append(p)
+
+for process in Processes:
+    process.join()
+
+
+finish1 = time.perf_counter()
+
+print(f'Finished in {round(finish1-start,2)} second(s)')
